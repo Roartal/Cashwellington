8faade890cca08c04fb5de9b6de1fb2f4af2a8e0
Markus K. Gruber <marki.gruber@googlemail.com>
2020-03-30 21:22:15 +0200

    Foundation
    Added first version of local unity project

---
+++ Assets/Rotate_On_Y_Axis.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate_On_Y_Axis : MonoBehaviour
+{
+    public Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newRotation = new Vector3(transform.eulerAngles.x, target.transform.eulerAngles.y,transform.eulerAngles.z);
+        this.transform.eulerAngles = newRotation;
+    }
+}
